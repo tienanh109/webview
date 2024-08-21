29c5606c9f5888e185eb176fad89e78a1b5f7401
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-08-21 04:28:11 +0000

    tienanh109

---
+++ app/src/main/java/com/tienanh109/webview/LocalActivity.java
@@ -0,0 +1,33 @@
+package com.tienanh109.webview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class LocalActivity extends Activity {
+
+	private WebView webview1;
+
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.local);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+
+	private void initialize(Bundle _savedInstanceState) {
+		webview1 = findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+
+		webview1.setWebViewClient(new WebViewClient() {
+			});
+	}
+
+	private void initializeLogic() {
+		webview1.loadUrl("file:///android_asset/index.html");
+	}
+}
